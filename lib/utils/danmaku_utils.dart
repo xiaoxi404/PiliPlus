e85c8b3dde1f07cdb4b684a797e7173991263829
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-11-02 12:37:20 +0800

    tweaks
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/utils/danmaku_utils.dart
+++ lib/utils/danmaku_utils.dart
@@ -1,22 +1,16 @@
-import 'package:canvas_danmaku/models/danmaku_content_item.dart';
-import 'package:flutter/material.dart';
+import 'dart:ui' show Color;
+
+import 'package:canvas_danmaku/models/danmaku_content_item.dart'
+    show DanmakuItemType;
 
 abstract class DmUtils {
-  static Color decimalToColor(int decimalColor) {
-    // 16777215 表示白色
-    int red = (decimalColor >> 16) & 0xFF;
-    int green = (decimalColor >> 8) & 0xFF;
-    int blue = decimalColor & 0xFF;
+  static Color decimalToColor(int decimalColor) =>
+      Color(decimalColor | 0xFF000000);
 
-    return Color.fromRGBO(red, green, blue, 1);
-  }
-
-  static DanmakuItemType getPosition(int mode) {
-    return switch (mode) {
-      4 => DanmakuItemType.bottom,
-      5 => DanmakuItemType.top,
-      7 => DanmakuItemType.special,
-      _ => DanmakuItemType.scroll,
-    };
-  }
+  static DanmakuItemType getPosition(int mode) => switch (mode) {
+    4 => DanmakuItemType.bottom,
+    5 => DanmakuItemType.top,
+    7 => DanmakuItemType.special,
+    _ => DanmakuItemType.scroll,
+  };
 }
