8bd3c22e417c63fb3f061a8e6325464a11008b0f
xiaoxi404 <1577912515@qq.com>
2025-12-16 11:49:01 +0800

    feat: vip cookie

--- lib/utils/storage_pref.dart
+++ lib/utils/storage_pref.dart
@@ -966,4 +966,7 @@ abstract final class Pref {
 
   static bool get saveReply =>
       _setting.get(SettingBoxKey.saveReply, defaultValue: true);
+
+  static String get vipCookie =>
+      _setting.get(SettingBoxKey.vipCookie, defaultValue: '');
 }
