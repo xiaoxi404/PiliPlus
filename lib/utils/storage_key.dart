25f6212ab66258b42aa84aa6a9796b7b4e182dcd
xiaoxi404 <1577912515@qq.com>
2025-12-16 11:49:01 +0800

    feat: vip cookie

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -230,6 +230,8 @@ abstract final class SettingBoxKey {
       tempPlayerConf = 'tempPlayerConf',
       reduceLuxColor = 'reduceLuxColor',
       liveCdnUrl = 'liveCdnUrl';
+
+  static const String vipCookie = 'vipCookie';
 }
 
 abstract final class LocalCacheKey {
